480307cf6cbdd976a51de25bd944e6d88479e734
Tinks18 <tinutani9@gmail.com>
2022-08-21 21:10:11 +0000

    Add code for updating guessed letter

--- run.py
+++ run.py
@@ -24,6 +24,16 @@ def lets_play(word):
                 print(guess, "is not in the word.")
                 tries -= 1
                 guessed_letters.append(guess)
+                else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
 
         elif len(guess) == len(word) and guess.isalpha():
 
