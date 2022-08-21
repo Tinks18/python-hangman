7ae466cf161a8155ec8257862c01c478a69ce0f2
Tinks18 <tinutani9@gmail.com>
2022-08-21 21:01:33 +0000

    Define function lets_play and add  varaibles

--- run.py
+++ run.py
@@ -4,3 +4,14 @@ from words import word_list
 def guess_word():
     final_word = random.choice(word_list)
     return word.upper()
+
+def lets_play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("\n Let's play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
