70d50778aa3d62e25fea64d3a2907c34177a7a80
Tinks18 <tinutani9@gmail.com>
2022-08-23 14:17:10 +0000

    Add function to retrieve data from google sheets

--- run.py
+++ run.py
@@ -1,147 +1,57 @@
+import gspread
+from google.oauth2.service_account import Credentials
 import random
-from words import word_list
 
-
-def guess_word():
-    word = random.choice(word_list)
-    return word.upper()
-
-
-def lets_play(word):
-    word_completion = "_" * len(word)
-    guessed = False
-    guessed_letters = []
-    guessed_words = []
-    tries = 6
-    print("\n Let's play Hangman!")
-    print(display_hangman(tries))
-    print(word_completion)
-    print("\n")
-    while not guessed and tries > 0:
-        guess = input("Please guess a letter or word: ").upper()
-        if len(guess) == 1 and guess.isalpha():
-            if guess in guessed_letters:
-                print("You already guessed the letter", guess)
-            elif guess not in word:
-                print(guess, "is not in the word.")
-                tries -= 1
-                guessed_letters.append(guess)
-            else:
-                print("Good job,", guess, "is in the word!")
-                guessed_letters.append(guess)
-                word_as_list = list(word_completion)
-                inds = [i for i, letter in enumerate(word) if letter == guess]
-                for index in inds:
-                    word_as_list[index] = guess
-                word_completion = "".join(word_as_list)
-                if "_" not in word_completion:
-                    guessed = True
-
-        elif len(guess) == len(word) and guess.isalpha():
-            if guess in guessed_words:
-                print("You already guessed the word", guess)
-            elif guess != word:
-                print(guess, "is not the word.")
-                tries -= 1
-                guessed_words.append(guess)
-            else:
-                guessed = True
-                word_completion = word
-        else:
-            print("Not a valid guess.")
-        print(display_hangman(tries))
-        print(word_completion)
-        print("\n")
-    if guessed:
-        print("Congrats, you guessed the word! You win!")
-    else:
-        print("Sorry, you ran out of tries.The word was " + word)
-
-
-def display_hangman(tries):
-    stages = [  # final state: head, torso, both arms, and both legs
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / \\
-                   -
-                """,
-                # head, torso, both arms, and one leg
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     /
-                   -
-                """,
-                # head, torso, and both arms
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |
-                   -
-                """,
-                # head, torso, and one arm
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|
-                   |      |
-                   |
-                   -
-                """,
-                # head and torso
-                """
-                   --------
-                   |      |
-                   |      O
-                   |      |
-                   |      |
-                   |
-                   -
-                """,
-                # head
-                """
-                   --------
-                   |      |
-                   |      O
-                   |
-                   |
-                   |
-                   -
-                """,
-                # initial empty state
-                """
-                   --------
-                   |      |
-                   |
-                   |
-                   |
-                   |
-                   -
-                """
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
     ]
-    return stages[tries]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_hangman')
+
+easy = SHEET.worksheet('easy')
+
+intermediate = SHEET.worksheet('intermediate')
+hard = SHEET.worksheet('hard')
+data = easy.get_all_records()
+
+# val = easy.acell('A2').value
+def sheet_value():
+    x = [item for item in intermediate.col_values(1) if item]
+    return list(x)
+
+secret = random.choice(sheet_value())
+print(secret)
 
 
-def main():
-    player_name = input("\n Please enter your name: ")
-    print(" \n Hello  " + player_name)
-    word = guess_word()
-    lets_play(word)
-    while input("Play Again? (Y/N) ").upper() == "Y":
-        word = guess_word()
-        lets_play(word)
 
 
-if __name__ == "__main__":
-    main()
+# list_of_dicts = worksheet.get_all_records()
+# data1 = intermediate.get_all_values()
+# data2 = hard.get_all_values()
+# print(data)
+# print(val)
+# print(data1)
+# print(data2)
+
+def get_level_data():
+    """
+    Get level input from the user.
+    """
+    print("Choose from Levels: Easy , Intermediate, Hard")
+
+    data_str = input("Please enter level:  ").upper()
+    print(f"The level you chose is {data_str}")
+
+# use_sheet = get_level_data()
+
+# def sheet_value():
+#     x = [item for item in use_sheet.col_values(1) if item]
+#     return list(x)
+
+# secret = random.choice(sheet_value())
+# print(secret)
